ca83632ed2874d2a1fa1b98bfd1a4b176842d99e
Axwell-austin <85233788+Axwell-austin@users.noreply.github.com>
2023-11-15 10:29:32 +0300

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _putchar - prints/writes character
+ * _putchar - prints or writes character
  * @character: The character to print
  * Return: 1, success, -1 error
  */
