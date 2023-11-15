97afde3ad673bb0386d9b455dfa426db8fa98b63
Axwell-austin <85233788+Axwell-austin@users.noreply.github.com>
2023-11-15 10:31:24 +0300

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -1,4 +1,4 @@
-/****************** PRINT POINTER ******************/
+/******** PRINT POINTER *******/
 /**
  * print_pointer - Prints the value of a pointer variable
  * @types: List a of arguments
